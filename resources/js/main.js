93e3495a92fddf7c5de5dc0de97e74ef54dcd4c2
Erwan Riou <riou@viamundus.com>
2018-01-09 23:49:58 +0100

    trying to implement the jquery function to uptade text on side bar when moove the draggable item

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
-
-
   //Jquery draggable fucntion
+  $("#krd").draggable({
+    grid: [ 190, 0 ],
+    cursor: "move",
+    containment: '.selector',
+    drag: function( event, ui ) {
+      if(ui.position.left = 190) {
+        $(".bluebox h2").text("3.000 Kr");
+      } 
+    }
+  });
 
-    $( "#krd" ).draggable({
-      grid: [ 190, 0 ],
-      cursor: "move",
-      containment: '.selector'
 
-    });
+  //Tooltip function on top of the formulary
+  $(".fa-question-circle").hover(function() {
+    $(this).next(".popup").toggle();
+  });
+
+
+
+
 
 
 
