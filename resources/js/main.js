58cb9d2f24d5932833a7a4b85b314b3376d5ce97
Erwan Riou <riou@viamundus.com>
2018-01-10 02:51:29 +0100

    page finalized

--- resources/js/main.js
+++ resources/js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     },
     //Part of the fonction that create the slider and modify the text in the html
     slide: function( e, ui ) {
-
+        $(".formCTA input").val("Lån " + (ui.value/1000) + ".000 kr »");
       if (ui.value == 3000) {
         $(".bluebox h2").text("3.000 Kr");
         $(".bluebox p").text("Tilbagebetales efter 30 dage: 3.625 kr.");
@@ -30,7 +30,6 @@ $(document).ready(function(){
         $(".bluebox p").text("Tilbagebetales efter 30 dage: 7.130 kr.");
         $(".textbox p").text("Et lån på kr. 6.000 i 30 dage har en etableringsomkostning på kr. 1.130 , en ÅOP på 716,11 % samt en fast debitorrente på 0%. Det betyder, at ydelsen og den samlede tilbagebetaling efter 30 dage på kr. 7.130.");
       }
-
     }
   });
 
